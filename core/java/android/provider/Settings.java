2166ff69667c3194ae214572b8f2bbffaaa51c14
Altaf-Mahdi <altaf.mahdi@gmail.com>
2019-02-16 15:56:14 -0500

    Hide the power menu on the lockscreen [1/2]
    Change-Id: Ia76e2c06d460ce6d0bc3c2dff9decdf5423159e9

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4217,6 +4217,13 @@ public final class Settings {
          */
         public static final String SCREENSHOT_DEFAULT_MODE = "screenshot_default_mode";
 
+         /**
+         * Whether to display the power menu dialog while on the lockscreen
+         * @hide
+         */
+        public static final String POWERMENU_LOCKSCREEN = "powermenu_lockscreen";
+
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
