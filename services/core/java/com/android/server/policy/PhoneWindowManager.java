216ffd7d4a77e11ee70441e2441ccdca9a2a829c
Stylogey <stylogey@gmail.com>
2019-03-20 22:24:11 +0100

    OP Gestures: Fix navbar detection and landscape mode

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2665,12 +2665,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             updateRotation(true);
         }
 
-        if (mUseGestureButton && !mGestureButtonRegistered) {
+        if ((mUseGestureButton || !mNavbarVisible) && !mGestureButtonRegistered) {
             mGestureButton = new GestureButton(mContext, this);
             mWindowManagerFuncs.registerPointerEventListener(mGestureButton);
             mGestureButtonRegistered = true;
         }
-        if (mGestureButtonRegistered && !mUseGestureButton) {
+        if ((!mUseGestureButton || mNavbarVisible) && mGestureButtonRegistered) {
             mWindowManagerFuncs.unregisterPointerEventListener(mGestureButton);
             mGestureButtonRegistered = false;
         }
