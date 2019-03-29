e6765f82da3aa1b533455a9b0ddcd081ea1b7039
wirmpolter <wirmpolter@gmail.com>
2019-03-29 16:07:42 -0400

    Revert "Extended audio panel [1/2]"
    This reverts commit a5661a485497b960696daadc0944012bc54c88be.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4239,23 +4239,6 @@ public final class Settings {
          */
         public static final String POWERMENU_LOCKSCREEN = "powermenu_lockscreen";
 
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_MEDIA = "audio_panel_view_media";
-
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_RINGER = "audio_panel_view_ringer";
-
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_NOTIFICATION = "audio_panel_view_notification";
-
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_ALARM = "audio_panel_view_alarm";
-
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_VOICE = "audio_panel_view_voice";
-
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_BT_SCO = "audio_panel_view_bt_sco";
 
         /**
          * Weather the device introduction is completed
