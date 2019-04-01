7912acb64774b0c9e14af63879c22be2c97e0706
AgentFabulous <kshitijgm@gmail.com>
2019-04-01 18:27:02 -0400

    QS: Allow using frameworks resources exclusively [1/2]
    - We can load the framework drawable/color and apply that in order to
      restore the default QS color scheme.
    - Make it play well with our internal themer
    
    Change-Id: Ia705d4d6583bfc7c5b3e6b0e19fff12487000f5d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4218,6 +4218,16 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * Whether to use framework styles and resources on qs panel
+         * @hide
+         */
+        public static final String QS_PANEL_BG_USE_FW = "qs_panel_bg_use_fw";
+
+        /** @hide */
+        private static final Validator QS_PANEL_BG_USE_FW_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
          * Whether to show the battery info on the lockscreen while charging
          * @hide
          */
@@ -4396,6 +4406,7 @@ public final class Settings {
             QS_PANEL_BG_COLOR,
             QS_PANEL_BG_COLOR_WALL,
             QS_PANEL_BG_USE_WALL,
+            QS_PANEL_BG_USE_FW,
         };
 
         /**
@@ -4516,6 +4527,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_PANEL_BG_COLOR);
             PRIVATE_SETTINGS.add(QS_PANEL_BG_COLOR_WALL);
             PRIVATE_SETTINGS.add(QS_PANEL_BG_USE_WALL);
+            PRIVATE_SETTINGS.add(QS_PANEL_BG_USE_FW);
         }
 
         /**
@@ -4611,6 +4623,7 @@ public final class Settings {
             VALIDATORS.put(QS_PANEL_BG_COLOR, QS_PANEL_BG_COLOR_VALIDATOR);
             VALIDATORS.put(QS_PANEL_BG_COLOR_WALL, QS_PANEL_BG_COLOR_WALL_VALIDATOR);
             VALIDATORS.put(QS_PANEL_BG_USE_WALL, QS_PANEL_BG_USE_WALL_VALIDATOR);
+            VALIDATORS.put(QS_PANEL_BG_USE_FW, QS_PANEL_BG_USE_FW_VALIDATOR);
         }
 
         /**
