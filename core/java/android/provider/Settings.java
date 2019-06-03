899381d67053c9ab4a6e5cf4fc0203982702ca30
Thecrazyskull <anaskarbila@gmail.com>
2019-06-03 16:38:15 -0400

    base: Introduce Accidental Touch
    Accidental Touch is a feature useful for many, especially the gamers out there.
    It prevents any sort of accidental touch on hardware buttons while the touchscreen is
    in use.
    This means you won’t experience any unexpected behaviour while playing games,
    web browsing or even when attempting to reach your finger out to the edge of
    that humongous display.
    
    Change-Id: I1d4e83f7c8ad9ceeb1f8c1bd097047b7ab992a57

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4421,6 +4421,13 @@ public final class Settings {
         public static final String FONT_PACKAGES = "font_packages";
 
         /**
+         * Indicates whether ANBI (Accidental navigation button interaction) is enabled.
+         *
+         * @hide
+         */
+        public static final String ANBI_ENABLED_OPTION = "anbi_enabled_option";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
