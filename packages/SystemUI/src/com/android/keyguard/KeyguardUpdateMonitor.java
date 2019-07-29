c2672407d158185df2b14a9e0613a63725fa7a18
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-07-28 21:58:57 -0400

    KeyguardHostView: Auto face unlock v2
    Dismiss keyguard automatically when face is recognized, updated to pie
    
    pixys edits:-
    * Update our older patch
    * Also revert AospExtended/platform_frameworks_base@d055d0c
    
    Change-Id: If45c01af8885d57b90980551f929309101b7929d

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -252,6 +252,9 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
     private static final int HW_UNAVAILABLE_TIMEOUT = 3000; // ms
     private static final int HW_UNAVAILABLE_RETRY_MAX = 3;
 
+    // For face unlock identification
+    private String lastBroadcastActionReceived;
+
     private PocketManager mPocketManager;
     private boolean mIsDeviceInPocket;
     private final IPocketCallback mPocketCallback = new IPocketCallback.Stub() {
@@ -874,11 +877,16 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
         }
     };
 
+    public boolean isFaceTrusted(){
+        return lastBroadcastActionReceived.equals(ACTION_FACE_UNLOCK_STOPPED);
+    }
+
     private final BroadcastReceiver mBroadcastAllReceiver = new BroadcastReceiver() {
 
         @Override
         public void onReceive(Context context, Intent intent) {
             final String action = intent.getAction();
+            lastBroadcastActionReceived = action;
             if (AlarmManager.ACTION_NEXT_ALARM_CLOCK_CHANGED.equals(action)) {
                 mHandler.sendEmptyMessage(MSG_TIME_UPDATE);
             } else if (Intent.ACTION_USER_INFO_CHANGED.equals(action)) {
