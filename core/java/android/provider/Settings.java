ca6be0ff2c1c54c41af471d92f803f603732e3a9
wirmpolter <wirmpolter@gmail.com>
2019-08-03 02:11:51 -0400

    Merge branch 'pie-gsi' of https://android.googlesource.com/platform/f…
    …rameworks/base into HEAD

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8590,7 +8590,9 @@ public final class Settings {
             MANUAL_RINGER_TOGGLE_COUNT,
             HUSH_GESTURE_USED,
             VOLUME_LINK_NOTIFICATION,
-            STATUS_BAR_BATTERY_STYLE
+            STATUS_BAR_BATTERY_STYLE,
+            LOCK_SCREEN_ALLOW_PRIVATE_NOTIFICATIONS,
+            LOCK_SCREEN_SHOW_NOTIFICATIONS
         };
 
         /**
