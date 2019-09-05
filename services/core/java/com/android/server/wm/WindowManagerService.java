0f026b176f97db3498ee1bec5e006f77f5ffbcaf
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-04 23:12:03 -0400

    fwb: Port extended screenshot function from OOS
    Change-Id: I856597056cb56d479a7a2c47dfdfc199da4302b8

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -7469,6 +7469,20 @@ public class WindowManagerService extends IWindowManager.Stub
                 return UserHandle.USER_NULL;
             }
         }
+
+        @Override
+        public boolean isMinimizedDock() {
+            boolean isMinimizedDock;
+            synchronized (mWindowMap) {
+                try {
+                    boostPriorityForLockedSection();
+                    isMinimizedDock = getDefaultDisplayContentLocked().getDockedDividerController().isMinimizedDock();
+                } finally {
+                    resetPriorityAfterLockedSection();
+                }
+            }
+            return isMinimizedDock;
+        }
     }
 
     void registerAppFreezeListener(AppFreezeListener listener) {
@@ -7638,5 +7652,15 @@ public class WindowManagerService extends IWindowManager.Stub
     @Override
     public boolean isGestureButtonRegion(int i, int i2) {
         return this.mPolicy.isGestureButtonRegion(i, i2);
+    }
+
+    @Override
+    public void stopLongshotConnection() {
+        mPolicy.stopLongshotConnection();
+    }
+
+    @Override
+    public void takeOPScreenshot(int type, int reason) {
+        mPolicy.takeOPScreenshot(type, reason);
     }
 }
